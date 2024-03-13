314653fa8fd8e94d5d206f8aa680ea53c3bc9364
Fussita <hualong.chiang@gmail.com>
2024-03-13 11:08:50 -0400

    user_base

--- src/core-main.tsx
+++ src/core-main.tsx
@@ -1,9 +1,12 @@
+import { UserMain } from "./user_page/user-main"
 
 
 export const Core = () => {
     return (
         <div>
             CORE
+
+            <UserMain/>
         </div>
     )
 }
